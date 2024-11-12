300d5d07475375c3e05c5e3eed58317c93af941b
אלבוים נחמה <154@ladaat.local>
2024-11-12 16:54:01 +0200

    sorted by name - second file

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 for (let i = 0; i < catalog.length; i++) {
     catalog[i]["id"]=i+100;
 }
+function sortByName() {
+    let st=""
+    let name=document.getElementById("1").value;
+    let arr=catalog.filter(item=>item.title.includes(name));
+    for (const item of arr) {
+        st+="title: "+item.title+"</br>"
+        st+="artist: "+item.artist+"</br>"
+        st+="country: "+item.country+"</br>"
+        st+="company: "+item.company+"</br>"
+        st+="price: "+item.price+"</br>"
+        st+="year: "+item.year+"</br>"+"</br>"
+    }
+    document.getElementById("p1").innerHTML=st;
+}
