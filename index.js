e9fae6a348b563dcef2a0419a79473024e992a7b
אלבוים נחמה <154@ladaat.local>
2024-11-12 18:30:11 +0200

    add disk - 6th file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ for (let i = 0; i < catalog.length; i++) {
     catalog[i]["id"] = i + 100;
 }
 function sortByName() {
-    document.getElementById("t").innerHTML ="";
+    document.getElementById("t").innerHTML = "";
     let name = document.getElementById("1").value;
     let arr = catalog.filter(item => item.title.includes(name));
     for (const item of arr) {
@@ -19,7 +19,7 @@ for (const item of arrCountries) {
     document.getElementById("sel").innerHTML += "<option>" + item + "</option>"
 };
 function sortByCountry() {
-    document.getElementById("t").innerHTML ="";
+    document.getElementById("t").innerHTML = "";
     let country = event.target.value;
     let arr = catalog.filter(item => item.country === country);
     for (const item of arr) {
@@ -28,7 +28,7 @@ function sortByCountry() {
     document.getElementById("p1").innerHTML = st;
 }
 function findByYears() {
-    document.getElementById("t").innerHTML ="";
+    document.getElementById("t").innerHTML = "";
     st = ""
     let y1 = document.getElementById("y1").value;
     let y2 = document.getElementById("y2").value;
@@ -36,6 +36,25 @@ function findByYears() {
     myTable(disk)
 }
 function myTable(d) {
-    document.getElementById("t").innerHTML += "<table>"+"<tr>"+"<td>"+d.title+"</td>"+"<td>"+d.artist+"</td>"+"<td>"+d.country+"</td>"+"<td>"+d.company+"</td>"+"<td>"+d.price+"</td>"+"<td>"+d.year+"</td>"+"</tr"
+    document.getElementById("t").innerHTML += "<table>" + "<tr>" + "<td>" + d.title + "</td>" + "<td>" + d.artist + "</td>" + "<td>" + d.country + "</td>" + "<td>" + d.company + "</td>" + "<td>" + d.price + "</td>" + "<td>" + d.year + "</td>" + "</tr"
 
+}
+function addDisk() {
+    const disk = {
+        id: catalog[catalog.length - 1].id + 1,
+        title: document.getElementById("title").value,
+        artist: document.getElementById("artist").value,
+        country: document.getElementById("country").value,
+        company: document.getElementById("company").value,
+        price: document.getElementById("price").value,
+        year: document.getElementById("year").value
+    };
+    catalog.push(disk);
+    document.getElementById("title").value = ""
+    document.getElementById("artist").value = ""
+    document.getElementById("country").value = ""
+    document.getElementById("company").value = ""
+    document.getElementById("price").value = ""
+    document.getElementById("year").value = ""
+    alert("הדיסק נוסף בהצלחה!");
 }
